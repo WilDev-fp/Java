399f8cd40772239d1dd903ee6ef5aa8c783421bf
wi11iamFernandez <william.fernandez@edu.itspiemonte.it>
2024-11-15 15:29:44 +0100

    create project spring boot viaggio da solo

---
+++ viaggio-da-solo/src/main/java/com/fernandez/viaggiodasolo/ViaggioDaSoloApplication.java
@@ -0,0 +1,13 @@
+package com.fernandez.viaggiodasolo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViaggioDaSoloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViaggioDaSoloApplication.class, args);
+	}
+
+}
